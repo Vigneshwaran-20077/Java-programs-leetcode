638240d80c81ba271cd69a30c4521328fbee3f80
Vigneshwaran--2007 <vicky.vignesh.20077@gmail.com>
2026-07-23 08:42:04 +0530

    new

---
+++ Jewels.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Jewels {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        
+        for (char j : jewels.toCharArray()) {
+            jewelSet.add(j);
+        }
+        
+        int count = 0;
+        
+        for (char s : stones.toCharArray()) {
+            if (jewelSet.contains(s)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
